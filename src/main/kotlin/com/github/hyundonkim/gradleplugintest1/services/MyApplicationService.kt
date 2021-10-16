e70e0bf95db210aa61b637e86b49c7ce6aab81c1
GitHub Action <action@github.com>
2021-10-16 02:00:52 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/hyundonkim/gradleplugintest1/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.hyundonkim.gradleplugintest1.services
+
+import com.github.hyundonkim.gradleplugintest1.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/hyundonkim/gradleplugintest1/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hyundonkim/gradleplugintest1/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.hyundonkim.gradleplugintest1.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hyundonkim.gradleplugintest1.MyBundle
 
 class MyProjectService(project: Project) {
 
